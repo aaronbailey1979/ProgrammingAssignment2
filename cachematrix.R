7b3d938061359f717dc3999354b27f50cb8cd9d2
Benjamin Bailey <aaronbailey1979@gmail.com>
2024-03-31 13:56:37 -0400

    My code for programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -15,7 +15,7 @@
 
 ## 1.  set the value of the matrix
 ## 2.  get the value of the matrix
-## 3.  set the value of the inverse
+## 3.  set the value of the inverse 
 ## 4.  get the value of the inverse
 
 
